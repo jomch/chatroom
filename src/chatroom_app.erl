355adb23c774047ab0b451b15b34764142eb5fed
jom_ch <chenghui@outlook.com>
2017-07-18 17:28:55 +0800

    init

--- src/chatroom_app.erl
+++ src/chatroom_app.erl
@@ -12,9 +12,9 @@
 start(_Type, _Args) ->
 	Dispatch = cowboy_router:compile([
 		{'_', [
-			%%{"/", cowboy_static, {priv_file, websocket, "index.html"}},
-			{"/websocket", ws_handler, []}
-			%%{"/static/[...]", cowboy_static, {priv_dir, websocket, "static"}}
+			{"/", cowboy_static, {priv_file, chatroom, "index.html"}},
+			{"/websocket", ws_handler, []},
+			{"/static/[...]", cowboy_static, {priv_dir, chatroom, "static"}}
 		]}
 	]),
 	{ok, _} = cowboy:start_clear(http, [{port, 8088}], #{ env => #{dispatch => Dispatch} } ),
